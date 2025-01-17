302a720d8577c4f251a61b5e5c4778799bd871eb
zlkca <likzhang@gmail.com>
2025-01-17 09:41:27 -0500

    feat: merge folders

--- src/const.js
+++
@@ -1,6 +0,0 @@
-export const env = "dev"
-export const GoogleAuthApi = `https://www.googleapis.com/oauth2/v3` // userinfo?access_token=${user.access_token}`
-export const DynamoDBCfg = env == "dev" ? {endpoint: "http://localhost:8000"} : {region: "us-east-1"}
-
-
-   
